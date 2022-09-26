5e7ba16b07605ee3229eb86af7858aa13ed90bad
Nishkarsh-Mac <nishkarsh2912@gmail.com>
2022-09-26 16:59:39 +0530

    complete game script

--- script.js
+++ script.js
@@ -1,62 +1,77 @@
-const gameTitle=document.querySelector("#level-title")
-const allButtons=document.getElementsByClassName("btn")
+const gameTitle = document.querySelector("#level-title")
+const allButtons = document.getElementsByClassName("btn")
 
-const green=document.querySelector('#green')
-const red=document.querySelector('#red')
-const blue=document.querySelector('#blue')
-const yellow=document.querySelector('#yellow')
+const green = document.querySelector('#green')
+const red = document.querySelector('#red')
+const blue = document.querySelector('#blue')
+const yellow = document.querySelector('#yellow')
 
-const buttons=[red,green,blue,yellow]
-const colors=['red','green','yellow','blue']
-let computerPattern=[]
-let userPattern=[]
-let gameStarted=false
-let level=0
+const buttons = [red, green, blue, yellow]
+const colors = ['red', 'green', 'yellow', 'blue']
+let computerPattern = []
+let userPattern = []
+let gameStarted = false
+let level = 0
 
 //generating computer patterns and storing output
-function computerSequence(){
+function computerSequence() {
+    userPattern=[] //solution
     ++level
-    gameTitle.textContent=`Level: ${level}`
-    const randomNum=Math.floor(Math.random()*4)
+    gameTitle.textContent = `Level: ${level}`
+    const randomNum = Math.floor(Math.random() * 4)
     computerPattern.push(colors[randomNum])
     playSequence(colors[randomNum])
 }
 
 //for flashing the buttons and creating sounds
-function playSequence(colorName){
-    let audio= new Audio(`/sounds/${colorName}.mp3`)
+function playSequence(colorName) {
+    let audio = new Audio(`/sounds/${colorName}.mp3`)
     audio.play()
-    for (const color of buttons){
-        if(color.id==colorName){
-            color.style.visibility='hidden'
+    for (const color of buttons) {
+        if (color.id == colorName) {
+            color.style.visibility = 'hidden'
             setTimeout(() => {
-                color.style.visibility='visible'
+                color.style.visibility = 'visible'
             }, 100);
         }
     }
 }
 
-function tallySequence(){
-    for(let i=0;i<userPattern.length;++i){
-        if (userPattern[i]===computerPattern[i]){
-            
+function tallySequence(level) {
+        if (userPattern[level] === computerPattern[level]) {
+            if (userPattern.length === computerPattern.length) {
+                setTimeout(() => {
+                    computerSequence()
+                }, 1000);
+            }
         }
-    }
+        else {
+            document.body.style.backgroundColor = 'red'
+            gameTitle.textContent = 'Press Any Key To Restart'
+            gameStarted = false
+            let wrong = new Audio('/sounds/wrong.mp3')
+            setTimeout(()=>{
+               wrong.play() 
+            },300)
+        }
 }
 
+
 //tracking and storing user pattern
-for (const button of allButtons){
-    button.addEventListener('click',(e)=>{
+for (const button of allButtons) {
+    button.addEventListener('click', (e) => {
         userPattern.push(e.target.id)
         playSequence(e.target.id)
-        console.log(userPattern);
+        tallySequence(userPattern.length-1)
     })
 }
 
-if(gameStarted==false){
-    document.addEventListener('keypress',function startGame(){
+
+//add event listeners to during first game start
+if (gameStarted == false) {
+    document.addEventListener('keypress', function startGame() {
+        document.removeEventListener('keypress', startGame)
+        gameStarted = true
         computerSequence()
-        document.removeEventListener('keypress',startGame)
-        gameStarted==true
     })
 }
