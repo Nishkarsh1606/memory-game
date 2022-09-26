2eb0529bb4e9535e264cbac2bc3a5d5e058f65bf
NishkarshS-iMac <ns513@student.london.ac.uk>
2022-09-26 13:48:27 +0530

    simon game

---
+++ script.js
@@ -0,0 +1,5 @@
+const gameTitle=document.querySelector("#level-title")
+const green=document.querySelector('#green')
+const red=document.querySelector('#red')
+const blue=document.querySelector('#blue')
+const yellow=document.querySelector('#yellow')
